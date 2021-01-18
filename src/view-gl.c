caca664bf6245d6992b0216befc4f87d37e0d6ee
Samuel <samuel.cuella@gmail.com>
2021-01-18 21:20:26 +0100

    Fixup shaders for OpenGL ES

--- src/view-gl.c
+++ src/view-gl.c
@@ -167,6 +167,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
